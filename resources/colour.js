87a0ca19ca9e2868008bac3dab6981a6ced97afd
Tristan-Miller <tristan.finlayson@gmail.com>
2023-11-30 17:43:21 +1100

    Initial commit

---
+++ resources/colour.js
@@ -0,0 +1,73 @@
+let backgroundColor, textColor;
+
+function getRandomColor() {
+    // Generate a random color in hexadecimal format
+    return '#' + Math.floor(Math.random()*16777215).toString(16);
+  }
+  
+  function calculateContrast(color1, color2) {
+    // Calculate the contrast ratio between two colors
+    const luminance1 = calculateLuminance(color1);
+    const luminance2 = calculateLuminance(color2);
+  
+    const ratio = (Math.max(luminance1, luminance2) + 0.05) / (Math.min(luminance1, luminance2) + 0.05);
+    return ratio;
+  }
+  
+  function calculateLuminance(color) {
+    // Calculate the relative luminance of a color
+    const rgb = parseInt(color.slice(1), 16);
+    const r = (rgb >> 16) & 0xff;
+    const g = (rgb >>  8) & 0xff;
+    const b = (rgb >>  0) & 0xff;
+  
+    const srgb = [r / 255, g / 255, b / 255];
+  
+    for (let i = 0; i < srgb.length; i++) {
+      if (srgb[i] <= 0.03928) {
+        srgb[i] /= 12.92;
+      } else {
+        srgb[i] = Math.pow((srgb[i] + 0.055) / 1.055, 2.4);
+      }
+    }
+  
+    const luminance = 0.2126 * srgb[0] + 0.7152 * srgb[1] + 0.0722 * srgb[2];
+    return luminance;
+  }
+  
+  
+
+function changeColors() {
+
+
+    // Generate random colors until the contrast ratio is sufficient
+    do {
+      backgroundColor = getRandomColor();
+      textColor = getRandomColor();
+    } while (calculateContrast(backgroundColor, textColor) < 4.5);
+  
+    const themeContainer = document.getElementById('themeContainer');
+    themeContainer.style.backgroundColor = backgroundColor;
+    themeContainer.style.color = textColor;
+  }
+
+  function setInitalColours() {
+    backgroundColor = "#bfbfbf";
+    textColor = "#0000ff"
+    themeContainer.style.backgroundColor = backgroundColor;
+    themeContainer.style.color = textColor;
+}
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var themeContainer = document.getElementById('themeContainer');
+    
+    // Change colors every 3 seconds
+    //setInterval(changeColors, 3000);
+  
+    // Initial color change
+   // changeColors();
+   setInitalColours();
+
+  });
+  
+  
---
+++ resources/work.js
@@ -0,0 +1,68 @@
+function initializeMovingDivs(movingDivClass, tooltipId) {
+  const movingDivs = document.getElementsByClassName(movingDivClass);
+  let isHovered = false;
+  const hoverElement = document.querySelector("." + movingDivClass);
+  const tooltipElement = document.getElementById(tooltipId);
+
+  hoverElement.addEventListener("mousemove", function (event) {
+    tooltipElement.style.display = "block";
+    tooltipElement.style.left = event.clientX + "px";
+    tooltipElement.style.top = event.clientY + "px";
+  });
+
+  hoverElement.addEventListener("mouseout", function () {
+    tooltipElement.style.display = "none";
+  });
+
+  for (const movingDiv of movingDivs) {
+    movingDiv.addEventListener("mouseover", () => {
+      isHovered = true;
+    });
+
+    movingDiv.addEventListener("mouseout", () => {
+      isHovered = false;
+    });
+
+    function moveDiv() {
+      const speed = 1;
+      let x = Math.floor(Math.random() * window.innerWidth / 1.8);
+      let y = Math.floor(Math.random() * window.innerHeight / 1.8);
+
+      function update() {
+        if (!isHovered) {
+          const rect = movingDiv.getBoundingClientRect();
+
+          if (x < 0 || x + rect.width > window.innerWidth) {
+            speedX = -speedX;
+          }
+
+          if (y < 0 || y + rect.height > window.innerHeight) {
+            speedY = -speedY;
+          }
+
+          x += speedX;
+          y += speedY;
+
+          movingDiv.style.left = x + "px";
+          movingDiv.style.top = y + "px";
+        }
+
+        requestAnimationFrame(update);
+      }
+
+      let speedX = speed * (Math.random() > 0.5 ? 1 : -1);
+      let speedY = speed * (Math.random() > 0.5 ? 1 : -1);
+
+      update();
+    }
+
+    moveDiv();
+  }
+}
+
+// Call the function for each set of elements
+initializeMovingDivs("movingWorkOne", "sydneyFestival");
+initializeMovingDivs("movingWorkTwo", "weel");
+initializeMovingDivs("movingWorkThree", "rolus");
+initializeMovingDivs("movingWorkFour", "youi");
+initializeMovingDivs("movingWorkFive", "personal");
